58c737bf4230438a3b4cb3b1a30ef1e4965d8dc4
zainabmasoudi <zainab.masoudi@yahoo.com>
2025-12-08 23:17:59 +0000

    Initial secure Terraform commit

---
+++ main.tf
@@ -0,0 +1,78 @@
+# Creating S3 Bucket 
+# Terraform internal name: mybucket and aws dosen't see this name. 
+# the actual name is the bucketname
+resource "aws_s3_bucket" "main" {
+    bucket = var.bucketname
+
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }   
+}
+
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_acl" "acl_config" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership,
+    aws_s3_bucket_public_access_block.public_access,
+  ]
+ 
+  bucket = aws_s3_bucket.main.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.main.id
+  key  = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
+
+}
+
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.main.id
+  key  = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
+
+}
+
+/*resource "aws_s3_object" "profile" {
+  bucket = aws_s3_bucket.main.id
+  key = "profile.png"
+  source = "profile.png"
+  acl = "public-read"
+}
+*/
+
+resource "aws_s3_bucket_website_configuration" "portfolio_website" {
+  bucket = aws_s3_bucket.main.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+  depends_on = [ 
+    aws_s3_bucket_acl.acl_config
+   ]
+
+}
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "bucketname" {
+    default = "s3-backet-expriment"
+}
